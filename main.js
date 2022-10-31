119bb8646955c31252d8b9723ccd9614235bf24f
+ <+>
2022-10-31 04:43:20 -0400

    commit tree 6e624d2530b56a02804c72559dc2ca47d11ad5f6

--- main.js
+++ main.js
@@ -1,10 +1,58 @@
 // Defining text characters for the empty and full hearts for you to use later.
 const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
+const errorModal = document.querySelector("#modal")
+errorModal.classList.add('hidden')
+
 
 // Your JavaScript code goes here!
 
+document.addEventListener("DOMContentLoaded", () => {
+  console.log("DOM CONTENT LOADED")
+  errorModal.classList.add("hidden")
 
+  //call find likes within the DOMcontentloaded to add eventlisteners
+  //findLikes()
+  clickListener()
+})
+
+function hideError(){
+  errorModal.classList.add("hidden")
+}
+
+function findLikes(){
+  const likeArr = document.querySelectorAll(".like-glyph")
+
+  likeArr.forEach((singularLike) => {
+    singularLike.addEventListener("click", () => console.log("You found me"))
+  })
+}
+
+function clickListener(){
+  document.addEventListener('click', (event) => {
+    if(event.target.classList[0] === 'like-glyph'){
+      mimicServerCall()
+      .then(resp => {
+        const activated = event.target.classList.contains("activated-heart");
+        if(activated){
+          event.target.classList.remove("activated-heart")
+          event.target.innerHTML = EMPTY_HEART
+        }else {
+          event.target.classList.add("activated-heart")
+          event.target.innerHTML = FULL_HEART
+        }
+        //document.getElementById("like-glyph").innerHTML = FULL_HEART
+        
+      })
+      .catch(error => {
+        errorModal.classList.remove("hidden")
+        setTimeout(() => {
+          hideError()
+        }, 3000)
+      })
+    }
+  })
+}
 
 
 //------------------------------------------------------------------------------
